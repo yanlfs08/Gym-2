6d87f7aa06450ae8acf97638f3125f1fa7a67a0c
Yan Santos <yanlfs@hotmail.com>
2019-05-11 17:53:15 -0300

    frm login

--- src/view/FrameLogin.java
+++ src/view/FrameLogin.java
@@ -33,7 +33,6 @@ public class FrameLogin extends javax.swing.JFrame {
         txtSenha = new JPassWordFieldHint (new JPasswordField(), "", "  Senha");
  ;
         JBLogin1 = new javax.swing.JButton();
-        JBSair = new javax.swing.JButton();
         JLrecuperarSenha = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
@@ -102,30 +101,9 @@ public class FrameLogin extends javax.swing.JFrame {
             }
         });
 
-        JBSair.setBackground(new java.awt.Color(217, 81, 51));
-        JBSair.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
-        JBSair.setForeground(new java.awt.Color(255, 255, 255));
-        JBSair.setText("Sair");
-        JBSair.setBorderPainted(false);
-        JBSair.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        JBSair.setFocusPainted(false);
-        JBSair.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                JBSairMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                JBSairMouseExited(evt);
-            }
-        });
-        JBSair.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                JBSairActionPerformed(evt);
-            }
-        });
-
-        JLrecuperarSenha.setBackground(new java.awt.Color(217, 81, 51));
+        JLrecuperarSenha.setBackground(new java.awt.Color(10, 173, 255));
         JLrecuperarSenha.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        JLrecuperarSenha.setForeground(new java.awt.Color(217, 81, 51));
+        JLrecuperarSenha.setForeground(new java.awt.Color(10, 173, 255));
         JLrecuperarSenha.setText("Recuperar");
         JLrecuperarSenha.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         JLrecuperarSenha.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -146,59 +124,55 @@ public class FrameLogin extends javax.swing.JFrame {
 
         jLabel6.setBackground(new java.awt.Color(0, 0, 0));
         jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icones/icons8-customer-80 (1).png"))); // NOI18N
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icones/icons8-dumbbell-80 (1).png"))); // NOI18N
 
-        jLabel7.setBackground(new java.awt.Color(0, 0, 0));
-        jLabel7.setFont(new java.awt.Font("Segoe UI", 3, 22)); // NOI18N
-        jLabel7.setText("Login");
+        jLabel7.setBackground(new java.awt.Color(10, 173, 255));
+        jLabel7.setFont(new java.awt.Font("Segoe UI", 1, 22)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(10, 173, 255));
+        jLabel7.setText("GymSys");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(73, Short.MAX_VALUE)
-                .addComponent(jLabel5)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(JLrecuperarSenha)
-                .addGap(82, 82, 82))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(99, 99, 99)
+                        .addGap(85, 85, 85)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtCPF)
+                            .addComponent(txtSenha)
+                            .addComponent(JBLogin1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel5)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(JLrecuperarSenha)
+                                .addGap(0, 0, Short.MAX_VALUE))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(139, 139, 139)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(txtCPF, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txtSenha, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(JBLogin1, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(JBSair, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(148, 148, 148)
-                        .addComponent(jLabel7))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(137, 137, 137)
-                        .addComponent(jLabel6)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(jLabel7)
+                            .addComponent(jLabel6))))
+                .addContainerGap(70, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addComponent(jLabel7)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 21, Short.MAX_VALUE)
-                .addComponent(jLabel6)
-                .addGap(18, 18, 18)
+                .addGap(35, 35, 35)
+                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(38, 38, 38)
                 .addComponent(txtCPF, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(txtSenha, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(35, 35, 35)
+                .addGap(37, 37, 37)
                 .addComponent(JBLogin1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(JBSair, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(JLrecuperarSenha)
-                    .addComponent(jLabel5))
-                .addGap(71, 71, 71))
+                    .addComponent(jLabel5)
+                    .addComponent(JLrecuperarSenha))
+                .addContainerGap(52, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -209,7 +183,7 @@ public class FrameLogin extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 461, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 396, Short.MAX_VALUE)
         );
 
         pack();
@@ -233,12 +207,8 @@ public class FrameLogin extends javax.swing.JFrame {
     }//GEN-LAST:event_JLrecuperarSenhaMouseEntered
 
     private void JLrecuperarSenhaMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JLrecuperarSenhaMouseExited
-        JLrecuperarSenha.setForeground(new Color(217, 81, 51));
+        JLrecuperarSenha.setForeground(new Color(10,173,255));
     }//GEN-LAST:event_JLrecuperarSenhaMouseExited
-
-    private void JBSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBSairActionPerformed
-        System.exit(0);
-    }//GEN-LAST:event_JBSairActionPerformed
 
     private void JBLogin1MouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JBLogin1MouseEntered
         JBLogin1.setBackground(Color.white);
@@ -246,18 +216,6 @@ public class FrameLogin extends javax.swing.JFrame {
         JBLogin1.setBorderPainted(true);
         
     }//GEN-LAST:event_JBLogin1MouseEntered
-
-    private void JBSairMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JBSairMouseEntered
-        JBSair.setBackground(Color.white);
-        JBSair.setForeground(new Color(217,81,51));
-        JBSair.setBorderPainted(true);
-    }//GEN-LAST:event_JBSairMouseEntered
-
-    private void JBSairMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JBSairMouseExited
-        JBSair.setBackground(new Color(217,81,51));
-        JBSair.setForeground(Color.WHITE);
-        JBSair.setBorderPainted(false);
-    }//GEN-LAST:event_JBSairMouseExited
 
     private void JBLogin1MouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JBLogin1MouseExited
         JBLogin1.setBackground(new Color(10,173,255));
@@ -340,7 +298,6 @@ public class FrameLogin extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton JBLogin1;
-    private javax.swing.JButton JBSair;
     private javax.swing.JLabel JLrecuperarSenha;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
